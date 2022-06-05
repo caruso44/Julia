ba295c5f397d0e5a6ba6c54cf058360b12e7a1d4
matheus0andrade <matheubarreto@hotmail.com>
2022-06-05 20:23:15 -0300

    change count_unique_words.jl

--- Projeto/test_count_unique_words.jl
+++ Projeto/test_count_unique_words.jl
@@ -2,10 +2,8 @@ using Printf
 using BenchmarkTools
 using LegacyStrings
 
-fullstrip(str::AbstractString, chars::AbstractString) = replace(str, Regex("[$chars]") => "")
-
-punctuation_characters = r"~\`!@#$%^&*()_-+=[{]}\\|;:',<.>/?1234567890"
-#punctuation_characters = String("~\`!@#$%^&*()_-+=[{]}\\|;:',<.>/?1234567890")
+punctuation_characters = r"[~\`!@#\$%^&*()_\-\+\\|;:',<.>\/?1234567890]"
+#punctuation_characters = String("~\`!@#\$%^&*()_\-\+\\|;:',<.>\/?1234567890")
 #punctuation_characters = ASCIIString("~\`!@#$%^&*()_-+=[{]}\\|;:',<.>/?1234567890")
 #punctuation_characters = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c'
 
@@ -15,13 +13,16 @@ Opens the given file and makes a collection of its unique words.
 """
 function countwords(filename::String)
     openedfile = open(filename)
-    wordlist = String[]
+    wordlist = Set()
     for line in eachline(openedfile)
-        words = split( fullstrip(line, punctuation_characters))
-        map(word -> push!(wordlist, lowercase(word)), words)
+        words = split( replace(lowercase(line), punctuation_characters=>""))
+        for word in words
+            push!(wordlist, word)
+        end
     end
     close(openedfile)
     filter!(!isempty, wordlist)
+    println(wordlist)
     println("Number of unique words: ", length(wordlist))
 end
 
